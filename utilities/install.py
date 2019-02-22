3aa738a964a36e8678ce6d885f58c19bfbe8c048
rodriguez-facundo <bruseghini.f@gmail.com>
2019-02-22 19:02:37 -0300

    minor refactor to install from geppetto-application

--- utilities/install.py
+++ utilities/install.py
@@ -4,11 +4,11 @@ import sys
 from shutil import copyfile
 
 branch = None
-PYGEPPETTO = 'https://github.com/openworm/pygeppetto.git'
 NETPYNE = 'https://github.com/Neurosim-lab/netpyne.git'
+PYGEPPETTO = 'https://github.com/openworm/pygeppetto.git'
+APPLICATION = 'https://github.com/MetaCell/geppetto-hnn.git'
 JUPYTER = 'https://github.com/openworm/org.geppetto.frontend.jupyter.git'
-FRONTEND = 'https://github.com/openworm/org.geppetto.frontend.git'
-EXTENSION = 'https://github.com/MetaCell/geppetto-hnn.git'
+
 # by default clones branch (which can be passed as a parameter python install.py branch test_branch)
 # if branch doesnt exist clones the default_branch
 
@@ -51,61 +51,46 @@ if __name__ == "__main__":
     main(sys.argv[1:])
 
 os.chdir(os.getcwd() + "/../")
-# Cloning Repos
-clone(repository=PYGEPPETTO, 
-    folder='pygeppetto', 
-    default_branch='development')
-subprocess.call(['pip', 'install', '-e', '.'], cwd='./pygeppetto/')
 
-clone(repository=NETPYNE, 
-    folder='netpyne', 
-    default_branch='ui')
-subprocess.call(['pip', 'install', '-e', '.'], cwd='./netpyne/')
+# # Cloning Repos
+# clone(repository=PYGEPPETTO, 
+#     folder='pygeppetto', 
+#     default_branch='development'
+# )
+# subprocess.call(['pip', 'install', '-e', '.'], cwd='./pygeppetto/')
 
+# clone(repository=NETPYNE, 
+#     folder='netpyne', 
+#     default_branch='ui'
+# )
+# subprocess.call(['pip', 'install', '-e', '.'], cwd='./netpyne/')
 
-clone(repository=JUPYTER, 
-    folder='org.geppetto.frontend.jupyter', 
-    default_branch='refactor-sync')
-subprocess.call(['npm', 'install'], cwd='./org.geppetto.frontend.jupyter/js')
-subprocess.call(['npm', 'run', 'build-dev'], cwd='./org.geppetto.frontend.jupyter/js')
+# clone(repository=JUPYTER, 
+#     folder='org.geppetto.frontend.jupyter', 
+#     default_branch='development'
+# )
+# subprocess.call(['npm', 'install'], cwd='./org.geppetto.frontend.jupyter/js')
+# subprocess.call(['npm', 'run', 'build-dev'], cwd='./org.geppetto.frontend.jupyter/js')
 
-# subprocess.call(['git', 'submodule', 'update', '--init'], cwd='./')
-clone(repository=FRONTEND, 
-    folder='geppetto', 
-    default_branch='refactor-upgrade-materialui', 
-    cwdp='hnn_ui/', 
-    recursive=False, 
-    destination_folder='geppetto')
-    
-# checkout('geppetto', 'development','org.geppetto.frontend.jupyter/src/jupyter_geppetto/')
-clone(repository=EXTENSION, 
-    folder='geppetto-hnn', 
-    default_branch='development',
-    cwdp='hnn_ui/geppetto/src/main/webapp/extensions/')
-
-print("Enabling Geppetto HNN Extension ...")
-geppetto_configuration = os.path.join(os.path.dirname(__file__), './utilities/GeppettoConfiguration.json')
-copyfile(geppetto_configuration, './hnn_ui/geppetto/src/main/webapp/GeppettoConfiguration.json')
-
-# Installing and building
+# clone(repository=APPLICATION, 
+#     folder='geppetto-hnn', 
+#     default_branch='development'
+# )
 print("NPM Install and build for Geppetto Frontend  ...")
-subprocess.call(['npm', 'install'], cwd='./hnn_ui/geppetto/src/main/webapp/')
-subprocess.call(['npm', 'run', 'build-dev-noTest'], cwd='./hnn_ui/geppetto/src/main/webapp/')
+os.chdir(os.getcwd() + "/geppetto-hnn")
+subprocess.call(['npm', 'install'])
+subprocess.call(['npm', 'run', 'build-dev-noTest'])
 
 print("Installing jupyter_geppetto python package ...")
-subprocess.call(['pip', 'install', '-e', '.'], 
-                cwd='./org.geppetto.frontend.jupyter')
+os.chdir(os.getcwd() + "/../org.geppetto.frontend.jupyter")
+subprocess.call(['pip', 'install', '-e', '.'])
 print("Installing jupyter_geppetto Jupyter Extension ...")
-subprocess.call(['jupyter', 'nbextension', 'install', '--py', '--symlink', '--sys-prefix', 'jupyter_geppetto'],
-                cwd='./org.geppetto.frontend.jupyter')
-subprocess.call(['jupyter', 'nbextension', 'enable', '--py', '--sys-prefix', 'jupyter_geppetto'],
-                cwd='./org.geppetto.frontend.jupyter')
-subprocess.call(['jupyter', 'nbextension', 'enable', '--py', 'widgetsnbextension'],
-                cwd='./org.geppetto.frontend.jupyter')
-subprocess.call(['jupyter', 'serverextension', 'enable', '--py', 'jupyter_geppetto'],
-                cwd='./org.geppetto.frontend.jupyter')
+subprocess.call(['jupyter', 'nbextension', 'install', '--py', '--symlink', '--sys-prefix', 'jupyter_geppetto'])
+subprocess.call(['jupyter', 'nbextension', 'enable', '--py', '--sys-prefix', 'jupyter_geppetto'])
+subprocess.call(['jupyter', 'nbextension', 'enable', '--py', 'widgetsnbextension'])
+subprocess.call(['jupyter', 'serverextension', 'enable', '--py', 'jupyter_geppetto'])
 
 print("Installing HNN UI python package ...")
-subprocess.call(['pip', 'install', '-e', '.', '--no-deps', '--ignore-requires-python'], cwd='.')
-
-subprocess.call(['nrnivmodl', './hnn_ui/mod'], cwd='.')
+os.chdir(os.getcwd() + "/../")
+subprocess.call(['pip', 'install', '-e', '.', '--no-deps', '--ignore-requires-python'])
+subprocess.call(['nrnivmodl', './hnn_ui/mod'])
