1898ebe06942f820ca62a7c612ea9e8e474e817c
Afonso Pinto <afonsobspinto@gmail.com>
2019-03-13 11:47:35 +0000

    Change plot colors

--- hnn_ui/holoviews_plots.py
+++ hnn_ui/holoviews_plots.py
@@ -1,28 +1,26 @@
 import numpy as np
 import holoviews as hv
-from holoviews import opts, dim
-import pandas as pd
 from bokeh.resources import CDN
 from bokeh.embed import file_html
 from bokeh.layouts import layout
 hv.extension('bokeh')
 
 
-def get_dipole():
+def _plot(color1, color2):
     
     x = np.linspace(0, 4*np.pi, 100)
     y = np.sin(x)
 
     scatter1 = hv.Scatter((x, y), label='sin(x)')
-    scatter2 = hv.Scatter((x, y*2), label='2*sin(x)').opts(color='orange')
-    scatter3 = hv.Scatter((x, y*3), label='3*sin(x)').opts(color='green')
-    scatter4 = hv.Scatter(scatter3).opts(line_color='green', marker='square', fill_alpha=0, size=5)
+    scatter2 = hv.Scatter((x, y*2), label='2*sin(x)').opts(color=color1)
+    scatter3 = hv.Scatter((x, y*3), label='3*sin(x)').opts(color=color2)
+    scatter4 = hv.Scatter(scatter3).opts(line_color=color2, marker='square', fill_alpha=0, size=5)
 
     curve1 = hv.Curve(scatter1)
-    curve2 = hv.Curve(scatter2).opts(line_dash=(4, 4), color='orange')
-    curve3 = hv.Curve(scatter3).opts(color='orange')
+    curve2 = hv.Curve(scatter2).opts(line_dash=(4, 4), color=color1)
+    curve3 = hv.Curve(scatter3).opts(color=color1)
 
-    example = scatter1 *  curve1 * curve2 * scatter4 * curve3
+    example = scatter1 * curve1 * curve2 * scatter4 * curve3
 
     example.relabel("Dipole Dummy Example")
 
@@ -33,18 +31,21 @@ def get_dipole():
     return html
 
 
-def get_traces():
+def get_dipole():
+    return _plot('green', 'orange')
 
-    return get_dipole()
+
+def get_traces():
+    return _plot('blue', 'yellow')
 
 
 def get_psd():
-    return get_dipole()
+    return _plot('red', 'black')
 
 
 def get_raster():
-    return get_dipole()
+    return _plot('gray', 'green')
 
 
 def get_spectrogram():
-    return get_dipole()
+    return _plot('brown', 'blue')
