d773f70817ce9210a9055b3c4aa27e1f6a7a8131
Afonso Pinto <afonsobspinto@gmail.com>
2019-07-10 17:08:16 +0100

    Update install.py

--- utilities/install.py
+++ utilities/install.py
@@ -58,7 +58,7 @@ subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
 # Cloning Repos
 clone(repository=PYGEPPETTO, 
     folder='pygeppetto', 
-    default_branch='development'
+    default_branch='v0.5.0-alpha'
 )
 subprocess.call(['pip', 'install', '-e', '.'], cwd='./pygeppetto/')
 
@@ -70,14 +70,14 @@ subprocess.call(['pip', 'install', '-e', '.'], cwd='./netpyne/')
 
 clone(repository=JUPYTER, 
     folder='org.geppetto.frontend.jupyter', 
-    default_branch='development'
+    default_branch='v1.0.0-alpha'
 )
 subprocess.call(['npm', 'install'], cwd='./org.geppetto.frontend.jupyter/js')
 subprocess.call(['npm', 'run', 'build-dev'], cwd='./org.geppetto.frontend.jupyter/js')
 
 clone(repository=APPLICATION, 
     folder='geppetto-hnn', 
-    default_branch='development'
+    default_branch='v0.1.1'
 )
 print("NPM Install and build for Geppetto Frontend  ...")
 os.chdir(os.getcwd() + "/geppetto-hnn")
